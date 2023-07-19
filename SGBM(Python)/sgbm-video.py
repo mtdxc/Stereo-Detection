2cdbb964364e972a056b49fb667e19de75cfc1cc
cqm <mtdxc@126.com>
2023-07-19 15:52:07 +0800

    添加目标追踪算法

--- SGBM(Python)/sgbm-video.py
+++ SGBM(Python)/sgbm-video.py
@@ -3,6 +3,18 @@ import numpy as np
 import time
 import math
 
+import argparse
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str,
+	help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+	help="OpenCV object tracker type")
+ap.add_argument("-s", "--stereo", type=str, default="sgbm", 
+    help="stereo method: sgbm or bm")
+ap.add_argument("-c", "--cuda", type=bool, default=True, help="use cuda")
+args = vars(ap.parse_args())
+
 # -----------------------------------双目相机的基本参数---------------------------------------------------------
 #   left_camera_matrix          左相机的内参矩阵
 #   right_camera_matrix         右相机的内参矩阵
@@ -61,8 +73,27 @@ cv2.namedWindow(WIN_NAME, cv2.WINDOW_AUTOSIZE)
 # 读取视频
 fps = 0.0
 ret, frame = capture.read()
-useSGBM = True
-useCuda = True
+useSGBM = args["stereo"] == "sgbm"
+useCuda = args["cuda"]
+print('use stereo method', args["stereo"], ",cuda=", useCuda)
+print('use tracker', args["tracker"])
+# initialize a dictionary that maps strings to their corresponding
+# OpenCV object tracker implementations
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv2.TrackerCSRT_create,
+    "kcf": cv2.TrackerKCF_create,
+    #"boosting": cv2.TrackerBoosting_create,
+    "mil": cv2.TrackerMIL_create,
+    #"tld": cv2.TrackerTLD_create,
+    #"medianflow": cv2.TrackerMedianFlow_create,
+    #"mosse": cv2.TrackerMOSSE_create
+}
+# grab the appropriate object tracker using our dictionary of
+# OpenCV object tracker objects
+# tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+# initialize the bounding box coordinates of the object we are going to track
+initBB = None
+
 while ret:
     # 开始计时
     t1 = time.time()
@@ -158,19 +189,46 @@ while ret:
     # 计算出的threeD，需要乘以16，才等于现实中的距离
     threeD = threeD * 16
 
+    if initBB is not None:
+        # grab the new bounding box coordinates of the object
+        (success, box) = tracker.update(frame1)
+        # check to see if the tracking was a success
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  
+            middle_x = x + int(w / 2)
+            middle_y = y + int(h / 2)
+            try:
+                xyz = threeD[middle_x][middle_y]
+                # print("世界坐标是：", xyz[0], xyz[1], xyz[2], "mm")
+                str = "(%.2f,%.2f,%.2f)" % (xyz[0] / 1000.0, xyz[1] / 1000.0, xyz[2] / 1000.0)
+                cv2.putText(frame, str, (x,y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            except:
+                print(middle_x, middle_y, "out of range")
+
     # 鼠标回调事件
     cv2.setMouseCallback(WIN_NAME, onmouse_pick_points, threeD)
-
     #完成计时，计算帧率
     fps = (fps + (1. / (time.time() - t1))) / 2
     frame = cv2.putText(frame, "fps= %.2f" % (fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("left", frame1)
     cv2.imshow(WIN_NAME, disp)  # 显示深度图的双目画面
-    # 若键盘按下q则退出播放
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
 
+    # 若键盘按下q则退出播放
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    elif key == ord("s"):
+		# select the bounding box of the object we want to track (make
+		# sure you press ENTER or SPACE after selecting the ROI)
+        initBB = cv2.selectROI("left", frame1, False, False)
+        print("selectROI", initBB)
+        tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+		# start OpenCV object tracker using the supplied bounding box
+		# coordinates, then start the FPS throughput estimator as well
+        tracker.init(frame1, initBB)
+    
 # 释放资源
 capture.release()
 
